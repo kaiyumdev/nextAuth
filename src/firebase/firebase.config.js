f7327e827447b3841e3ba993417029e96291eba2
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-31 20:27:53 +0600

    implement email password based authentication with firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDuGfO6Sjrbd83AX2UB5oeiREXk_I09ac",
+  authDomain: "nextauth-a0b54.firebaseapp.com",
+  projectId: "nextauth-a0b54",
+  storageBucket: "nextauth-a0b54.appspot.com",
+  messagingSenderId: "829528542044",
+  appId: "1:829528542044:web:5711256d2173285ff88f99",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
 import Head from "next/head";
 import { useSession } from "next-auth/react";
+import auth from "@/firebase/firebase.auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 const HomePage = () => {
   const { data: session } = useSession();
+  const [user, loading, error] = useAuthState(auth);
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+      {/* <h1 style={{ textAlign: "center", marginTop: "10%" }}>
         Welcome To Next Auth Home Page
       </h1>
       <h2 style={{ textAlign: "center" }}>
         Logged in user: {session?.user?.name}
-      </h2>
+      </h2> */}
       <h1 style={{ textAlign: "center", marginTop: "10%" }}>
         Welcome To Next Auth Home Page
       </h1>
-      <h2 style={{ textAlign: "center" }}>
-        Logged in user: {session?.user?.email}
-      </h2>
+      {user?.email && (
+        <h2 style={{ textAlign: "center" }}>Logged in user: {user?.email}</h2>
+      )}
     </div>
   );
 };
