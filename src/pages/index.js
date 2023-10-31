1a7f93facf83a85305afb5dbd0d2f99bceed2d83
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-31 18:48:40 +0600

    implements private route with nextjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,12 @@ const HomePage = () => {
       <h2 style={{ textAlign: "center" }}>
         Logged in user: {session?.user?.name}
       </h2>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Welcome To Next Auth Home Page
+      </h1>
+      <h2 style={{ textAlign: "center" }}>
+        Logged in user: {session?.user?.email}
+      </h2>
     </div>
   );
 };
